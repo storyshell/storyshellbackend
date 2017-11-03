6bb356a26261e0fe54954217b0de88a428ff5bf9
meghab16 <megha.banchhor6@gmail.com>
2017-11-04 00:40:31 +0530

    last time changes

--- routes/profile.js
+++ routes/profile.js
@@ -3,9 +3,9 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var router = express.Router();
 
-router.get('/profile', function(req, res,next) {
+router.get('/profile', function(req, res, next) {
 console.log('profile page opens');
-	res.render('profile');
+	res.render('profile',{title :'profile'});
 
 });
 
